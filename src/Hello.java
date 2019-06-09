c044f263f85fd2ed873ecec50d32e66cacadec95
prsjdk <prsjdk@126.com>
2019-06-09 13:12:06 +0800

    system master

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("helloworld!");
-        System.out.println("master!");
+        System.out.println("master444345!");
     }
 }
